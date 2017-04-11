09da7d04681d15e92c9283a44292cd991eb228e2
Rich Sage <rich.sage@gmail.com>
2017-04-11 14:00:40 +0100

    Add CC option.

--- SendSesEmail/config.js
+++ SendSesEmail/config.js
@@ -4,6 +4,7 @@ var config = {
     "templateBucket" : "my-bucket",
     "templateKey" : "Templates/Template.html",
     "targetAddress" : "my-email@domain.com",
+    "targetCCAddress" : "",
     "fromAddress": "Me <my-email@domain.com>",
     "defaultSubject" : "Email From {{name}}"
 }
